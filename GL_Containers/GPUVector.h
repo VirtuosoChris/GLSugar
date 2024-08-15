37505bfb2c1e0d9433e04671eed971f3bfaf0e8c
Christopher Pugh <chris@virtuosoengine.com>
2024-08-15 04:49:05 -0400

    Update GPUVector.h
    (untested) refactor of GPUVector to have template map interface and switch to immutable storage

--- GL_Containers/GPUVector.h
+++ GL_Containers/GPUVector.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template <typename T>
+template <typename T, bool MappedInterface = false>
 struct GPUVector
 {
     gl::Buffer buffer;
@@ -10,11 +10,15 @@ struct GPUVector
 
     const static inline std::size_t DefaultCapacity = 64u;
 
-    GPUVector(std::size_t capacityIn = DefaultCapacity, GLenum usageIn = GL_STATIC_DRAW) :
+    constexpr static GLenum NonMappedCreationFlagsDefault = GL_DYNAMIC_STORAGE_BIT;
+    constexpr static GLenum PersistentMapCreationDefaultFlags = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_DYNAMIC_STORAGE_BIT;
+    constexpr static GLenum PersistentMappingDefaultFlags = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
+
+    GPUVector(std::size_t capacityIn = DefaultCapacity, GLenum usageIn = MappedInterface ? PersistentMapCreationDefaultFlags : NonMappedCreationFlagsDefault) :
         capacity(capacityIn),
         usage(usageIn)
     {
-        buffer.Data(capacity * sizeof(T), nullptr, usage);
+        buffer.Storage(capacity * sizeof(T), nullptr, usage);
     }
 
     void clear()
@@ -22,24 +26,18 @@ struct GPUVector
         size = 0;
     }
 
+    void shrink_to_fit()
+    {
+        capacity = size;
+        reallocate();
+    }
+
     void reserve(std::size_t capacityIn)
     {
         if (capacityIn > capacity)
         {
-#if _DEBUG
-            //std::clog << "GPUBuffer Reserve " << capacity << " from " << capacityIn<< std::endl;
-            //std::clog << "\t buffer was " << buffer.name() << std::endl;
-#endif
-
             capacity = capacityIn;
-            gl::Buffer b;
-
-            b.Data(capacity * sizeof(T), nullptr, usage);
-
-            buffer.CopySubData(b, 0u, 0u, size * sizeof(T));
-            buffer = std::move(b);
-
-            //std::clog << "\t buffer is " << buffer.name() << std::endl;
+            reallocate();
         }
     }
 
@@ -51,9 +49,31 @@ struct GPUVector
             assert(size < capacity);
         }
 
-        buffer.SubData((size++) * sizeof(T), sizeof(T), &t);
+        if (MappedInterface)
+        {
+            _impl.ptr()[(size++)] = t;
+        }
+        else
+        {
+            buffer.SubData((size++) * sizeof(T), sizeof(T), &t);
+        }
     }
 
+    template<typename = std::enable_if_t<MappedInterface == true>>
+    const T& operator[] (const std::size_t i) const
+    {
+        assert(_impl.mappedPtr != nullptr);
+        return _impl.mappedPtr[i];
+    }
+
+    template<typename = std::enable_if_t<MappedInterface == true>>
+    T& operator[] (const std::size_t i)
+    {
+        assert(_impl.mappedPtr != nullptr);
+        return _impl.mappedPtr[i];
+    }
+
+    template<typename = std::enable_if_t<MappedInterface == false>>
     T operator[] (const std::size_t i) const
     {
         assert(i < size);
@@ -63,11 +83,76 @@ struct GPUVector
         return rval;
     }
 
+    template<typename = std::enable_if_t<MappedInterface == false>>
     void write(const T& value, const std::size_t& i)
     {
         assert(i < size);
         buffer.SubData(i * sizeof(T), sizeof(T), &value);
     }
+
+    template<typename = std::enable_if_t<MappedInterface>>
+    void map(GLenum flags = PersistentMappingDefaultFlags)
+    {
+        assert(_impl.mappedPtr == nullptr);
+        _impl.mappedPtr = (T*)buffer.Map(flags);
+        assert(_impl.mappedPtr != nullptr);
+        _impl.mapFlags = flags;
+    }
+
+    template<typename = std::enable_if_t<MappedInterface>>
+    void unmap()
+    {
+        assert(_impl.mappedPtr != nullptr);
+        buffer.Unmap();
+        _impl.mappedPtr = nullptr;
+        _impl.mapFlags = 0;
+    }
+
+private:
+
+    template<typename = std::enable_if_t<MappedInterface>>
+    bool remap()
+    {
+        if (_impl.mapFlags)
+        {
+            map(_impl.mapFlags);
+            return _impl.mappedPtr != nullptr;
+        }
+    }
+
+    template<typename = std::enable_if_t<MappedInterface == false>>
+    void remap() { ; }
+
+    void reallocate()
+    {
+        gl::Buffer b;
+
+        b.Storage(capacity * sizeof(T), nullptr, usage);
+
+        buffer.CopySubData(b, 0u, 0u, size * sizeof(T));
+        buffer = std::move(b);
+
+        remap();
+    }
+
+    template <bool MappedInterface>
+    struct _Impl
+    {
+        T* ptr() const { return nullptr; }
+    };
+
+    template<>
+    struct _Impl<true>
+    {
+        T* mappedPtr = nullptr;
+        GLenum mapFlags = 0;
+
+        T* ptr() { return mappedPtr; }
+    };
+
+    using Impl = _Impl<MappedInterface>;
+
+    Impl _impl;
 };
 
 
